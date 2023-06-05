c07b0cc75501cd6ba320509a1fa81e111201159e
Suman Sharma <48875541+SumanSharmaTech@users.noreply.github.com>
2023-06-05 21:56:57 +0545

    objets

---
+++ 2/object.sql
@@ -0,0 +1,20 @@
+
+// objects in dbms
+
+create type student_type as Object(
+    id int,
+    name varchar(20),
+    roll int
+)
+
+create table tbl_student of student_type
+
+desc tbl_student
+
+insert into tbl_student values(new student_type(1,'ram',5));
+insert into tbl_student values(new student_type(2,'acer',6));
+
+select * from tbl_student
+
+
+
